b1f3d888f0d76cc3d49f258681a6a38e946053ed
IKCSI <ejemplo.work@proton.me>
2023-06-26 16:22:01 +0200

    .

--- js/video.js
+++ js/video.js
@@ -1,3 +1,4 @@
+
 const videos = [
     './video/video1.mp4',
     './video/workshop.mp4',
@@ -7,6 +8,8 @@ const videos = [
 
 let currentVideo = 0;
 
+/* Código sin cambios aquí... */
+
 function changeVideo() {
     const videoElement = document.querySelector('#background-video');
     videoElement.src = videos[currentVideo];
@@ -15,11 +18,21 @@ function changeVideo() {
 
     // Desactiva la clase "active" para todos los círculos
     const circles = document.querySelectorAll('.circle');
-    circles.forEach(circle => circle.classList.remove('active'));
+    circles.forEach(circle => {
+        circle.classList.remove('active');
+        circle.style.width = '8px';  /* tamaño más pequeño para círculos no activos */
+        circle.style.height = '8px';  /* tamaño más pequeño para círculos no activos */
+    });
 
     // Activa la clase "active" para el círculo correspondiente al vídeo actual
     const currentCircle = document.querySelector(`#circle${currentVideo + 1}`);
     currentCircle.classList.add('active');
+    currentCircle.style.width = '24px';  /* tamaño más grande para círculos activos */
+    currentCircle.style.height = '24px';  /* tamaño más grande para círculos activos */
+
+/* Resto del código sin cambios aquí... */
+
+
     
     // Prepara el siguiente video
     currentVideo = (currentVideo + 1) % videos.length;
@@ -58,4 +71,39 @@ document.getElementById("navbar-toggler-icon").addEventListener('click', functio
         menuOpen = false;
     }
 });
+
+//Transición Logo
+
+function changeVideo() {
+    // Muestra el contenedor de transición
+    const transitionContainer = document.querySelector('#transition-container');
+    const logo = document.querySelector('#logo');
+    transitionContainer.classList.add('visible');
+    logo.classList.add('large');
+    
+    // Espera 1 segundo antes de cambiar el vídeo y ocultar el contenedor de transición
+    setTimeout(function() {
+        // Cambia el vídeo
+        const videoElement = document.querySelector('#background-video');
+        videoElement.src = videos[currentVideo];
+        videoElement.load();
+        videoElement.play();
+
+        // Desactiva la clase "active" para todos los círculos
+        const circles = document.querySelectorAll('.circle');
+        circles.forEach(circle => circle.classList.remove('active'));
+
+        // Activa la clase "active" para el círculo correspondiente al vídeo actual
+        const currentCircle = document.querySelector(`#circle${currentVideo + 1}`);
+        currentCircle.classList.add('active');
+        
+        // Prepara el siguiente video
+        currentVideo = (currentVideo + 1) % videos.length;
+
+        // Oculta el contenedor de transición y reduce el tamaño del logo
+        transitionContainer.classList.remove('visible');
+        logo.classList.remove('large');
+    }, 1000);
+}
+
 
---
+++ procesar_formulario.php
@@ -0,0 +1,28 @@
+
+<?php
+// Obtener los valores enviados desde el formulario
+$saludo = $_POST['title'];
+$nombre = $_POST['fname'];
+$apellido = $_POST['lname'];
+$email = $_POST['email'];
+$mensaje = $_POST['msg'];
+
+// Realizar cualquier validación necesaria aquí
+
+// Enviar el correo electrónico de respuesta
+$to = 'ignacio@pkwdriveclub.com';
+$subject = 'Nuevo mensaje de contacto';
+$message = "Saludo: $saludo\n";
+$message .= "Nombre: $nombre\n";
+$message .= "Apellido: $apellido\n";
+$message .= "Email: $email\n";
+$message .= "Mensaje: $mensaje\n";
+$headers = "From: $email\r\n";
+
+// Enviar el correo electrónico
+mail($to, $subject, $message, $headers);
+
+// Redireccionar a una página de confirmación o mostrar un mensaje de éxito
+header('Location: workshop.html');
+exit();
+?>
