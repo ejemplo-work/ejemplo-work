91ef0950331a3c4a4eabf8dabb1685fb4d4df0c1
IKCSI <134521514+ejemplo-work@users.noreply.github.com>
2023-08-25 13:29:15 +0200

    .

---
+++ relojes_show_files/strings.js
@@ -0,0 +1,8 @@
+var landingPageStrings = {
+    'countdown.days': 'Giorni alla scadenza',
+    'countdown.hours': 'Ore',
+    'countdown.minutes': 'Minuti',
+    'countdown.seconds': 'Secondi',
+    'datetime.openPicker': 'Seleziona una data',
+    end: null
+};
