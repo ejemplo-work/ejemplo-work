076f4130699b9b6cdeb4d2c7ca8d1d4f10673999
IKCSI <134521514+ejemplo-work@users.noreply.github.com>
2023-08-29 17:58:52 +0200

    pkw

--- relojes_show_files/Nueva carpeta con ítems/strings.js
+++
@@ -1,8 +0,0 @@
-var landingPageStrings = {
-    'countdown.days': 'Giorni alla scadenza',
-    'countdown.hours': 'Ore',
-    'countdown.minutes': 'Minuti',
-    'countdown.seconds': 'Secondi',
-    'datetime.openPicker': 'Seleziona una data',
-    end: null
-};
