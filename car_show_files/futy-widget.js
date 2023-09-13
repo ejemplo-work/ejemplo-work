aca15d1f0a8418ea4ed820108609fce9eaab3eb0
IKCSI <ejemplo.work@proton.me>
2023-09-13 17:40:36 +0200

    .

---
+++ car_show_files/futy-widget.js
@@ -0,0 +1,1 @@
+!function(e,t){window.Futy.meta="v: 1.0";var n=e.createElement(t);n.async=!0;var r=window.Promise&&window.fetch?"modern.js":"legacy.js";n.src="https://v1.widget.futy.io/js/futy-widget-"+r;var a=e.getElementsByTagName(t)[0];a.parentNode.insertBefore(n,a)}(document,"script");
